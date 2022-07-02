d8da8c5d59ace90594f33b2e60e743a822d2c1bd
Akash <kushwahaakash971gmail.com>
2022-07-03 02:23:06 +0530

    NavBar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import logo from './homepage/ProjectHero.svg';
+import { Container,
+    Typography,
+    Toolbar,
+    Button,
+    AppBar
+} from '@mui/material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
 
 function Navbar() {
+    
   return (
-    <div>Navbar</div>
+    <Container>
+        <AppBar color = 'inherit'>
+        <Toolbar>
+        <Typography style={{flexGrow: 1 }}> <img src={logo} alt="logo"   /></Typography>
+        <Button  color = 'inherit'>Plans</Button>
+        <Button color = 'inherit'>About Us</Button>
+        <Button endIcon={<ArrowDropDownIcon /> } color = 'inherit'>Support</Button>
+        <Button color = 'inherit'>FAQs</Button>
+        <Button variant = 'outlined'>Book Workers</Button>
+      </Toolbar>
+        </AppBar>
+    </Container>
   )
 }
 
